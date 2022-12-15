3ddffc0f9240d3814fb8093d6e8d829b094984e7
richelbilderbeek <richel@richelbilderbeek.nl>
2022-12-15 09:58:41 +0100

    Add updated script

---
+++ scripts/loerdagskurser.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Dör öppnas"' | at 10:00
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Vi starta"' | at 10:15
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Bygga upp nintendo"' | at 10:50
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tid att ta en paus"' | at 11:00
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tillbaks till arbete"' | at 11:15
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tiden är slut för dem flesta"' | at 12:00
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tillbaks till arbete"' | at 12:15
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tiden är slut"' | at 13:00
+
+
