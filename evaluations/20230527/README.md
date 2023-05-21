e1fabca705f4d63461541ccac4a74018e757608d
richelbilderbeek <richel@richelbilderbeek.nl>
2023-05-21 14:01:06 +0200

    Prepare protocol and interview questions

---
+++ evaluations/20230527/README.md
@@ -0,0 +1,28 @@
+# Utvärdering 2023-05-27
+
+Evaluation taken at 2022-12-10.
+ 
+ * [Results](results.md)
+ * The protocol used [.odt](protokoll.odt) [.pdf](protokoll.pdf)
+ * The evaluation form [.odg](utvardering.odg) [.pdf](utvardering.pdf) [.png](utvardering.png)
+ * Interview questions for students [.odt](questions.odt) [.pdf](questions.pdf)
+ * [Interview questions for parents](questions_parents.md)
+
+## Board
+
+ * Adult members: for free
+
+## Students
+
+ * Discord yes/no
+
+## Parents
+
+### Info
+
+ * Next year: member by default
+
+### Questions
+
+ * Communication: website good enough? Email list?
+
---
+++ scripts/hackathon_ums.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Dör öppet"' | at 12:00
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Klockan starta"' | at 12:15
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tid att ta en paus"' | at 13:00
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tillbaks till arbete"' | at 13:15
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tid att ta en paus"' | at 14:00
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tillbaks till arbete"' | at 14:15
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tid foer pizza"' | at 15:00
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tillbaks till arbete"' | at 15:30
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tid att ta en paus"' | at 16:15
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tillbaks till arbete"' | at 16:30
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tid att ta en paus"' | at 17:15
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tillbaks till arbete"' | at 17:30
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Klockan slut"' | at 18:00
+
+
